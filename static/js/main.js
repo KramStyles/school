1b72ebe3034c1e43f290856eb17fa20599cf9653
Michael Jamie <kramstyles@outlook.com>
2021-08-19 14:36:24 +0100

    Introduced Ajax with login... Found need to introduce Font awesome for spinning effect on buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,15 @@
 $('#btnLogin').click(function (e) {
     e.preventDefault();
     frm = $('#frmLogin');
+    btn = $(this);
+    btn.html("<i class='fa fa-cog fa-spin'></i> Processing...");
     $.ajax({
         url: '/sign_in',
         method: 'POST',
         data: frm.serialize(),
         success: function (data) {
             alert(data);
+            btn.html('Log In');
         }
     });
 });
